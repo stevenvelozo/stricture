311fbf456ea3bf62b9f5b0197b76000a4ff1ba0d
Steven Velozo <steven@velozo.com>
2015-09-03 08:53:45 -0700

    Started compiler

--- source/Stricture-Compile.js
+++ source/Stricture-Compile.js
@@ -17,11 +17,51 @@ var libLineReader = require('line-by-line');
  {
 	var tmpJSONFile = pFable.settings.OutputLocation+pFable.settings.OutputFileName+'.json';
 
+	var tmpTableCount = 0;
+	var tmpPropertyCount = 0;
+
 	console.info('--> Compiling MicroDDL to JSON');
 	console.log('  > Input file:  '+pFable.settings.InputFileName)
 	console.log('  > Output file: '+tmpJSONFile)
+
+	libFS.appendFileSync(tmpJSONFile, "{\n\t[\n");
+
+	// Parse the file line-by-line
 	var tmpLineReader = new libLineReader(pFable.settings.InputFileName);
-	//libFS.appendFileSync(tmpJSONFile, "\n    );\n");
+
+	tmpLineReader.on('error',
+		function (pError)
+		{
+			console.error('>>> Error reading MicroDDL file.');
+			console.log('  > '+pError);
+		}
+	);
+
+	tmpLineReader.on('line',
+		function (pLine)
+		{
+			tmpLineReader.pause();
+			console.log('  > Line read: '+pLine);
+			if (tmpPropertyCount > 0)
+			{
+				libFS.appendFileSync(tmpJSONFile, ',');
+			}
+
+			libFS.appendFileSync(tmpJSONFile, '\n\t\t"HullaBullshit": "'+pLine+'"');
+
+			tmpPropertyCount++;
+
+			tmpLineReader.resume();
+		}
+	);
+
+	tmpLineReader.on('end',
+		function ()
+		{
+			// All lines are read, file is closed now.
+			libFS.appendFileSync(tmpJSONFile, "\n\t]\n}\n");
+		}
+	);
 };
 
 module.exports = CompileMicroDDL;
